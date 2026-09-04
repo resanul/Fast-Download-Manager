fbb2a13fbcd43094ceb5300ebbf069f51983a6f4
Mir Resanul Karim <resanul@gmail.com>
2026-09-04 10:19:52 +0600

    feat: enable IDM-style download dialogs in app

--- fastdm/__main__.py
+++ fastdm/__main__.py
@@ -1,4 +1,7 @@
-from fastdm.ui import main
+from fastdm.ui import MainWindow, main
+from fastdm.advanced_ui import install
+
+install(MainWindow)
 
 if __name__ == '__main__':
     raise SystemExit(main())
