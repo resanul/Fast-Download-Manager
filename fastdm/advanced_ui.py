0750c8bb30bc3e075d5721e29cdfdf53e88fdfa2
Mir Resanul Karim <resanul@gmail.com>
2026-09-04 10:26:15 +0600

    fix: sort advanced UI imports

--- fastdm/advanced_ui.py
+++ fastdm/advanced_ui.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from pathlib import Path
 import uuid
+from pathlib import Path
 
 from .download_dialogs import DownloadFileInfoDialog, DownloadStatusDialog
 from .engine import DownloadTask
